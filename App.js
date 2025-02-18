7f559782213c2581f646b747b54c369f705b7563
Diego Costa <diegocosta.contato2021@gmail.com>
2025-02-18 12:00:10 -0300

    [feat]-Criando um componente.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PrimeiroComponente from './components/componente-teste';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world!</Text>
+      <PrimeiroComponente />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/componente-teste.js
@@ -0,0 +1,8 @@
+
+import { Text } from 'react-native';
+
+export default function PrimeiroComponente() {
+  return (
+    <Text>Eu sou o componente.</Text>
+  );
+}   
